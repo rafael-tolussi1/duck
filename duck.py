677eefe47dddd0850a2fea659ed5e90a13bd6552
Rafael Tolussi <rguames50@gmail.com>
2026-04-17 17:09:37 -0300

    Create duck.py

---
+++ duck.py
@@ -0,0 +1,68 @@
+import discord
+from discord.ext import commands
+import random
+import os
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Estamos logados como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Olá! eu sou um bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+@bot.command()
+async def meme(ctx):
+   
+    lista = os.listdir('images')
+
+    imagem = random.choice(lista)
+
+    with open(f'images/{imagem}', 'rb') as f:
+        #Vamos armazenar o arquivo convertido da biblioteca do Discord nesta variável!
+        picture = discord.File(f)
+    # Podemos então enviar esse arquivo como um parâmetro
+    await ctx.send(file=picture)
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Uma vez que chamamos o comando duck, o programa chama a função get_duck_image_url '''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+bot.run("")
+
+
+
+
